36e616691a27d0ab13c37eec94fb17325f42425e
Alex LaFroscia <alex@lafroscia.com>
2016-10-02 22:12:15 -0700

    Add testdouble-chai dependency

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-testdouble-chai'
+  name: 'ember-cli-testdouble-chai',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import(app.bowerDirectory + '/testdouble-chai/lib/testdouble-chai.js', {
+      exports: {
+        'testdouble-chai': ['default']
+      }
+    });
+  }
 };
