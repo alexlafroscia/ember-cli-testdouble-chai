b1d12ebbe38c0cb18432ae479cf4c14012d069a3
Alex LaFroscia <alex@lafroscia.com>
2016-10-11 21:23:52 -0700

    Prevent exposing testdouble-chai as a Bower dep

--- blueprints/ember-cli-testdouble-chai/index.js
+++ blueprints/ember-cli-testdouble-chai/index.js
@@ -2,9 +2,5 @@
 
 module.exports = {
   description: 'ember-cli-testdouble-chai installation blueprint',
-  normalizeEntityName: function() {},
-
-  beforeInstall: function() {
-    return this.addBowerPackageToProject('testdouble-chai', 'alexlafroscia/testdouble-chai#support-amd-modules');
-  }
+  normalizeEntityName: function() {}
 };
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
+
+var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+
+var tdChaiDirectory = path.dirname(require.resolve('testdouble-chai'));
 
 module.exports = {
   name: 'ember-cli-testdouble-chai',
-
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-
-    app.import(app.bowerDirectory + '/testdouble-chai/lib/testdouble-chai.js', {
-      exports: {
-        'testdouble-chai': ['default']
-      }
+  
+  // Grab the testdouble-chai file and add it to the vendor tree
+  treeForVendor(tree) {
+    var trees = [];
+    
+    if (tree) {
+      trees.push(tree)
+    }
+    
+    var tdChaiTree = new Funnel(tdChaiDirectory)
+    trees.push(tdChaiTree);
+    
+    return mergeTrees(trees);
+  },
+  
+  // Import the testdouble-chai module from the vendor tree
+  // NOTE: Only added to the test environment
+  included(app) {
+    var vendor = this.treePaths.vendor;
+    
+    app.import(vendor + '/testdouble-chai.js', {
+      exports: ['default'],
+      type: 'test'
     });
   }
 };
