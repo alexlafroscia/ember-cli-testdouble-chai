79eb8aebfb1b7efd0e58e2e73dfa4d49d945118e
Alex LaFroscia <alex@lafroscia.com>
2016-10-11 23:11:09 -0700

    Fix `test-helper` generator

--- blueprints/ember-cli-testdouble-chai/files/tests/test-helper.js
+++ blueprints/ember-cli-testdouble-chai/files/tests/test-helper.js
@@ -1,8 +1,9 @@
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
 
-import td, { tdChai } from 'testdouble';
 import chai from 'chai';
+import td from 'testdouble';
+import tdChai from 'testdouble-chai';
 
 chai.use(tdChai(td));
 
